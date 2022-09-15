4ad8cd468ae73cf230d9aafb5bf92f37e3effa7a
Roman Naumov <55456532+Novelzloy@users.noreply.github.com>
2022-09-15 20:42:11 +0300

    Update Contents.swift

--- testTaskForIron-Water-Studio.playground/Contents.swift
+++ testTaskForIron-Water-Studio.playground/Contents.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-func versionComparable(_ mainStr: String, _ comparableStr: String) throws -> ComparisonResult {
+func versionComparable(_ mainStr: String, _ comparableStr: String) -> ComparisonResult {
 
     let separator = "."
     var dotsCount = 0
@@ -20,6 +20,8 @@ func versionComparable(_ mainStr: String, _ comparableStr: String) throws -> Com
     let result = Array(repeating: "0.", count: dotsCount)
     comparableStrDotted.insert(contentsOf: result, at: 0)
     mainStrDotted.insert(contentsOf: result, at: 0)
+    print("\(mainStrDotted) \(comparableStrDotted)")
+//    print(result)
 
     let zeroDifferent = mainStrDotted.count - comparableStrDotted.count
     
@@ -55,13 +57,13 @@ extension ComparisonResult {
     }
 }
 
-var a = try versionComparable("01", "1")
-var b = try versionComparable("01.234.56", "2.0.0")
-var c = try versionComparable("1.2", "1.2.3")
-var d = try versionComparable(".0", "1")
-var e = try versionComparable("1.3", "1.2")
-var j = try versionComparable("1.3", "1.2.4")
-var f = try versionComparable(".1", "0.1")
+var a = versionComparable("01", "1")
+var b = versionComparable("01.234.56", "2.0.0")
+var c = versionComparable("1.2", "1.2.3")
+var d = versionComparable(".0", "1")
+var e = versionComparable("1.3", "1.2")
+var j = versionComparable("1.3", "1.2.4")
+var f = versionComparable(".1", "0.1")
 
 let results = [a,b,c,d,e,j,f].map { $0.result }
 for result in results {
