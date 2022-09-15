84fd7b8feeef21a0209fcfe54c564265ba83416e
Roman Naumov <55456532+Novelzloy@users.noreply.github.com>
2022-09-15 20:11:23 +0300

    Update Contents.swift

--- testTaskForIron-Water-Studio.playground/Contents.swift
+++ testTaskForIron-Water-Studio.playground/Contents.swift
@@ -3,12 +3,23 @@ import Foundation
 func versionComparable(_ mainStr: String, _ comparableStr: String) throws -> ComparisonResult {
 
     let separator = "."
+    var dotsCount = 0
 
     var mainStrDotted = mainStr.components(separatedBy: separator)
     var comparableStrDotted = comparableStr.components(separatedBy: separator)
     
     let mainStrDottedJoined = mainStrDotted.joined(separator: separator)
     let comparableStrDottedJoined = comparableStrDotted.joined(separator: separator)
+    
+    while comparableStrDotted.description.hasPrefix(".") || mainStrDotted.description.hasPrefix(".") {
+        comparableStrDotted.removeFirst()
+        mainStrDotted.removeFirst()
+        dotsCount += 1
+    }
+    
+    let result = Array(repeating: "0.", count: dotsCount)
+    comparableStrDotted.insert(contentsOf: result, at: 0)
+    mainStrDotted.insert(contentsOf: result, at: 0)
     
     let zeroDifferent = mainStrDotted.count - comparableStrDotted.count
     
@@ -49,8 +60,8 @@ var b = try versionComparable("01.234.56", "2.0.0")
 var c = try versionComparable("1.2", "1.2.3")
 var d = try versionComparable(".0", "1")
 var e = try versionComparable("1.3", "1.2")
-var j = try versionComparable("1.3", "1.2")
-var f = try versionComparable("1.2.3", "1.2.4")
+var j = try versionComparable("1.3", "1.2.4")
+var f = try versionComparable(".1", "0.1")
 
 
 
